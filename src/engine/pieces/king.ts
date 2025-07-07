f815087dfe61c497d099d13afc448fffcdf0d791
Donovan Lock <Donovan.Lock@softwire.com>
2025-07-07 13:43:52 +0100

    Implemented Red-7

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,13 +1,23 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+
+type Move = [row:number,col:number];
 
 export default class King extends Piece {
+    private moveList : Move[]= [[1,1],[1,0],[1,-1],[0,1],[0,-1],[-1,1],[-1,0],[-1,-1]];
     public constructor(player: Player) {
         super(player);
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currSquare = board.findPiece(this);
+        const moves = [];
+        for (let move of this.moveList) {
+            moves.push(Square.at(currSquare.row + move[0], currSquare.col + move[1]));
+        }
+
+        return moves;
     }
 }
--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,7 +1,6 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
