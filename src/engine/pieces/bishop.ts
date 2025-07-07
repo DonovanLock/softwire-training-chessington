d14337423bb912fb099225e8de6485c248b35921
Shreeka <Shreeka.Lohani@softwire.com>
2025-07-07 12:07:18 +0100

    removed unnecessary import

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,7 +1,6 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-import Square from '../square';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
