c877c8679e427b74a4af12aa885275dc3e9b3133
Shreeka <Shreeka.Lohani@softwire.com>
2025-07-07 13:37:49 +0100

    knight moves

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,13 +1,23 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+
+type Move = [row:number,col:number];
 
 export default class Knight extends Piece {
+    private moveList : Move[]= [[1,2],[1,-2],[-1,2],[-1,-2],[2,1],[-2,1],[2,-1],[-2,-1]];
     public constructor(player: Player) {
         super(player);
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currSquare = board.findPiece(this);
+        const moves = [];
+        for (let move of this.moveList) {
+            moves.push(Square.at(currSquare.row + move[0], currSquare.col + move[1]));
+        }
+
+        return moves;
     }
 }
