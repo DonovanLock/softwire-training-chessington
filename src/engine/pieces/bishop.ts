364c57956d91ec200e34b7887f3b9099847f85e0
Shreeka <Shreeka.Lohani@softwire.com>
2025-07-07 10:39:07 +0100

    implemented bishop moves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,30 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currSquare = board.findPiece(this);
+        const moves = [];
+        const row = currSquare.row;
+        const col = currSquare.col;
+        let i = 1;
+        while (row + i < 8 && col + i < 8) {
+            moves.push(Square.at(row + i, col + i));
+            i++;
+        }
+        i = 1;
+        while (row - i >= 0 && col + i < 8) {
+            moves.push(Square.at(row - i, col + i));
+            i++;
+        }
+        i = 1;
+        while (row - i >= 0 && col - i >= 0) {
+            moves.push(Square.at(row - i, col - i));
+            i++;
+        }
+        i = 1;
+        while (row + i < 8 && col - i >= 0) {
+            moves.push(Square.at(row + i, col - i));
+            i++;
+        }
+        return moves;
     }
 }
